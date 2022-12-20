b10a415d132baf2205211f59b3943ad82c58cadf
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-12-20 18:44:10 +0530

    page guard added to auth form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { Router, useRouter } from "next/router";
 import AuthForm from "../components/auth/AuthForm";
 import Navigation from "../components/layout/Navigation";
 
 export default function Home() {
+  // const router = useRouter();
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // // client side page guard, if user is logged in then redirect to dashboard page instead of login page
+  // useEffect(() => {
+  //   getSession().then(session => {
+  //     if(session){
+  //       router.replace('/dashboard');
+  //     }else{
+  //       setIsLoading(false);
+  //     }
+  //   })
+  // },[router]);
+
+  // if(isLoading){
+  //   return <p>Loading...</p>
+  // }
+
   return (
     <>
       <Navigation />
@@ -9,3 +30,21 @@ export default function Home() {
     </>
   );
 }
+
+// Server side page guard, if user is logged in then redirect to dashboard page instead of login page
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
