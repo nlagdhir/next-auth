6d85816eb45d01aa645c41ff2bc364b20ee9f326
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-12-20 18:33:03 +0530

    server side guard added when user not logged in then redirect from dashboard to login page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,33 +4,55 @@ import { useState, useEffect } from "react";
 import { getSession } from "next-auth/react";
 
 const Dashboard = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [loadedSession, setLoadedSession] = useState();
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [loadedSession, setLoadedSession] = useState();
+//   // client side page guard, if user not logged in then redirect to login page
+//   useEffect(() => {
+//     getSession().then((session) => {
+//       if (!session) {
+//         window.location.href = "/";
+//       } else {
+//         setIsLoading(false);
+//       }
 
-    useEffect(() => {
-        getSession().then((session) => {
-            if(!session){
-                window.location.href = "/";
-            }else{
-                setIsLoading(false);
-            }
+//       setLoadedSession(session);
+//     });
+//   }, []);
 
-            setLoadedSession(session);
-        })
-    },[])
+//   if (isLoading) {
+//     return <p>Loading...</p>;
+//   }
 
-    if(isLoading){
-        return <p>Loading...</p>;
-    }
+  return (
+    <>
+      {/* Navigation */}
+      <Navigation />
 
-    return <>
-        {/* Navigation */}
-        <Navigation />
-
-        {/* Sidebar */}
-        <Sidebar />
+      {/* Sidebar */}
+      <Sidebar />
     </>
-}
+  );
+};
+
+
+  // server side page guard, if user not logged in then redirect to login page
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
 
 export default Dashboard;
